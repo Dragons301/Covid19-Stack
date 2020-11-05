0d824339cc822f14e6f5577245bef22b6fb23de1
Abuhajer <Abuhagerr@gmail.com>
2020-11-05 12:40:24 +0200

    addTempHomePage

--- server.js
+++ server.js
@@ -6,9 +6,15 @@ const express = require ('express');
 const cors = require ('cors');
 const app = express();
 const PORT = process.env.PORT|| 3000;
+const superagent=require('superagent');
 
 app.use(cors());
+app.get('/',welcomePage);
 
+function welcomePage (request , respone)
+{
+  respone.send('Welcome to homepage');
+}
 
 app.listen (PORT , () => {
   console.log(`Listening on port: ${PORT}`);
